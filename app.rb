acb38fa8f6746f0c30076c6c272b18c18b6e23f0
aprilfaithdays <aprilfaith.e@gmail.com>
2020-02-18 13:34:19 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is April :)"
+    end
+
+    get '/hometown' do
+        "My hometown is Des Plaines, IL"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Pink Hair, Green Eyes' at the moment"
+    end
+
 end
